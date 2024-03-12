cef2e1babe2f686bc002c25efe34e5c414a96f8a
renatu <renatu_mb@yahoo.com.br>
2024-03-12 16:13:43 -0700

    Floating button to return to the top

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
 import Header from "./components/Header.tsx";
 import Main from "./components/Main.tsx";
 import Footer from "./components/Footer.tsx";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCoffee} from "@fortawesome/free-solid-svg-icons";
-import {faGoogle} from "@fortawesome/free-brands-svg-icons";
-import {RiCheckLine} from "@remixicon/react";
 
 function App() {
 
     return (
         <>
-            {/* TO TEST */}
-            <div className="hidden" >
-                test
-                <FontAwesomeIcon icon={faCoffee} spin={true}/>
-                opp
-                <FontAwesomeIcon icon={faGoogle} className="text-blue-800  size-16 bg-lime-600 rounded" />
-                ---
-                <RiCheckLine className="text-blue-800  size-16 bg-lime-600 rounded"></RiCheckLine>
-            </div>
-
             <Header/>
             <Main/>
             <Footer/>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,35 @@
-import {RiCloseLine, RiMenu3Line, RiMoonLine} from "@remixicon/react";
+import { RiArrowUpSFill, RiCloseLine, RiMenu3Line, RiMoonLine} from "@remixicon/react";
+import {useEffect} from "react";
 
 export default function Header() {
+
+    let lastEvent: number;
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            clearTimeout(lastEvent)
+            lastEvent = setTimeout(() => {
+                console.log(window.scrollY)
+
+                const btnUp = document.getElementById("scrollUpBtn")
+                const header = document.getElementById("header")
+
+                if (!btnUp || !header) return
+
+                if (window.scrollY > 300) {
+                    btnUp.classList.add("bottom-6")
+                    btnUp.classList.remove("-bottom-1/2")
+                    header.classList.add("border-b-secondaryColor")
+                    header.classList.add("border-b" )
+                } else {
+                    btnUp.classList.add("-bottom-1/2")
+                    btnUp.classList.remove("bottom-6")
+                    header.classList.remove("border-b")
+                    header.classList.remove("border-b-secondaryColor")
+                }
+            }, 500)
+        })
+    }, [])
 
     const toggleMobileMenu = () => {
         const mobileMenu = document.getElementById("mobile-menu")
@@ -9,7 +38,7 @@ export default function Header() {
     }
 
     return (
-        <header className=" bg-primaryColor fixed top-0 left-0 w-dvw z-50 ">
+        <header className=" bg-primaryColor fixed top-0 left-0 w-dvw z-50 transition-all duration-500 " id="header">
             <nav className="container relative w-full h-14 flex justify-between items-center ">
                 <div>
                     <a href="#" className="text-2xl uppercase font-oswald">Bur<span className="text-secondaryColor">guer</span></a>
@@ -30,6 +59,9 @@ export default function Header() {
                     <RiMenu3Line className="cursor-pointer md:hidden" onClick={toggleMobileMenu}/>
                 </div>
             </nav>
+            <a href="#" id="scrollUpBtn" className="fixed right-5 -bottom-1/2 bg-secondaryColor shadow-2xl inline-block p-4 rounded-full text-lg text-darkColor z-50 hover:-translate-y-1 ease-in duration-200">
+                <RiArrowUpSFill></RiArrowUpSFill>
+            </a>
         </header>
     )
 }
